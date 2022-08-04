e131b0c4222f18e087fd4ffaf175c1cfdb8c1b31
Abdelrhman Sayed El-Sadory <abdo_sadory@yahoo.com>
2022-08-05 01:12:31 +0200

    main-tag Section4-left is done

--- js/script.js
+++ js/script.js
@@ -23,3 +23,34 @@ $(".owl-carousel").owlCarousel({
     },
   },
 });
+
+var swiper = new Swiper(".mySwiper", {
+  loop: false,
+  spaceBetween: 20,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev ",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+
+var swiper = new Swiper(".mySwiper3", {
+  slidesPerView: 3,
+  grid: {
+    rows: 2,
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
